028e85befb56b1b6b6686e71980d852a4cbf7961
mattbzca <matthewtan@my.whccd.edu>
2025-04-02 14:18:41 -0700

    Created model and started editing controller

---
+++ COMP003B.Assignment-3/Controllers/EventController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment_3.Models;
+
+namespace COMP003B.Assignment_3.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP003B.Assignment-3/Models/EventRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment_3.Models
+{
+    public class EventRegistration
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string EventCode { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int Tickets {  get; set; }
+
+        public string ReferralCode {  get; set; }
+    }
+}
