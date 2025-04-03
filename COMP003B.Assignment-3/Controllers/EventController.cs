e87348b739046dff22444b33afe2f9962184b905
mattbzca <matthewtan@my.whccd.edu>
2025-04-02 23:49:18 -0700

    Finalized Nav, partial view, views, controllers, and model.

--- COMP003B.Assignment-3/Controllers/EventController.cs
+++ COMP003B.Assignment-3/Controllers/EventController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using COMP003B.Assignment_3.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace COMP003B.Assignment_3.Controllers
 {
@@ -7,12 +8,33 @@ namespace COMP003B.Assignment_3.Controllers
     [Route("event")]
     public class EventController : Controller
     {
-        [HttpGet("event/register/{eventCode}")]
+        [HttpGet("Event/Register/{eventCode}")]
         public IActionResult Register(string eventCode = "spring-2025")
         {
             return View();
         }
 
-        
+        [HttpGet("/Event/Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost("/Event/Register")]
+        public IActionResult Register([FromForm] EventRegistration Event) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(Event);
+            }
+
+            return RedirectToAction("Success", Event);
+        }
+
+        [Route("success")]
+        public IActionResult Success(EventRegistration Event)
+        {
+            return View(Event);
+        }
     }
 }
