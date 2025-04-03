ee564501106273b7cb6620d1a7aaf55493a82078
mattbzca <matthewtan@my.whccd.edu>
2025-04-02 21:49:50 -0700

    Added Views, Nav, trying to fix Controller

--- COMP003B.Assignment-3/Controllers/EventController.cs
+++ COMP003B.Assignment-3/Controllers/EventController.cs
@@ -3,11 +3,16 @@ using COMP003B.Assignment_3.Models;
 
 namespace COMP003B.Assignment_3.Controllers
 {
+    // Handles request that start with "/event".
+    [Route("event")]
     public class EventController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("event/register/{eventCode}")]
+        public IActionResult Register(string eventCode = "spring-2025")
         {
             return View();
         }
+
+        
     }
 }
--- COMP003B.Assignment-3/Models/EventRegistration.cs
+++ COMP003B.Assignment-3/Models/EventRegistration.cs
@@ -1,13 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace COMP003B.Assignment_3.Models
 {
     public class EventRegistration
     {
-        public int Id { get; set; }
-
         [Required]
-        [StringLength(100, MinimumLength = 5)]
+        [StringLength(100, MinimumLength = 3)]
         public string FullName { get; set; }
 
         [Required]
@@ -22,6 +21,6 @@ namespace COMP003B.Assignment_3.Models
         [Range(1, 10)]
         public int Tickets {  get; set; }
 
-        public string ReferralCode {  get; set; }
+        public string? ReferralCode {  get; set; }
     }
 }
